6be5fc11f2ae61a9b5f8743d6d7c06dcfcb854ec
Christian Häußler (Windows) <chrissx@chrissx.ga>
2020-04-10 03:30:21 +0200

    Implemented all of the caching and stuff.

--- SimpleHttpProxy/Program.cs
+++ SimpleHttpProxy/Program.cs
@@ -1,112 +1,197 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
+using static SimpleHttpProxy.Config;
+using static SimpleHttpProxy.Program;
 
 namespace SimpleHttpProxy
 {
-    class Config
-    {
-        public static int PORT = 3143; //this is an hommage to acng, which uses 3142
-    }
+	static class Config
+	{
+		public const int PORT = 3143; //this is an hommage to acng, which uses 3142
+		public const string ROOT_DIR = "/var/cache/likeu-cacher-ng/";
+		public const string CACHE_DIR = ROOT_DIR + "cache/";
+		public const string CACHE_DICT = CACHE_DIR + "_dict";
+		public static readonly Random RANDOM = new Random();
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var listener = new HttpListener();
-            listener.Prefixes.Add($"http://*:{Config.PORT}/");
-            listener.Start();
-            Console.WriteLine("Listening...");
-            while (true)
-                new Thread(new Relay(listener.GetContext()).ProcessRequest).Start();
-        }
-    }
+		/// <summary>
+		/// this is Environment.NewLine, but better
+		/// </summary>
+		public static char NewLine => '\n';
 
-    class Relay
-    {
-        private readonly HttpListenerContext originalContext;
+		//helper funcs
 
-        public Relay(HttpListenerContext originalContext)
-        {
-            this.originalContext = originalContext;
-        }
+		public static int FirstIndexOf(this string s, char c)
+		{
+			for (int i = 0; i < s.Length; i++)
+				if (s[i] == c)
+					return i;
+			return -1;
+		}
 
-        public void ProcessRequest() //if in cache read
-        {
-            string rawUrl = originalContext.Request.RawUrl;
-            if(rawUrl[0] == '/') rawUrl = rawUrl.Substring(1);
-            ConsoleUtilities.WriteRequest("Received request for: " + rawUrl);
+		public static void WriteAllLines(string path, IEnumerable<string> contents, Encoding encoding)
+		{
+			if (path == null)
+				throw new ArgumentNullException(nameof(path));
+			if (contents == null)
+				throw new ArgumentNullException(nameof(contents));
+			if (encoding == null)
+				throw new ArgumentNullException(nameof(encoding));
+			if (path.Length == 0)
+				throw new ArgumentException(nameof(path));
 
-            var relayRequest = (HttpWebRequest) WebRequest.Create(rawUrl);
-            relayRequest.KeepAlive = false;
-            relayRequest.Proxy.Credentials = CredentialCache.DefaultCredentials;
-            relayRequest.UserAgent = this.originalContext.Request.UserAgent;
-           
-            var requestData = new RequestState(relayRequest, originalContext);
-            relayRequest.BeginGetResponse(ResponseCallBack, requestData);
-        }
+			using var w = new StreamWriter(path, false, encoding);
+			foreach (string line in contents)
+				w.Write(line + NewLine);
+		}
 
-        static void ResponseCallBack(IAsyncResult asynchronousResult)
-        {
-            RequestState requestData = asynchronousResult.AsyncState;
-            ConsoleUtilities.WriteResponse("Got response from " + requestData.context.Request.RawUrl);
-            
-            using (var responseFromWebSite = (HttpWebResponse) requestData.webRequest.EndGetResponse(asynchronousResult))
-            {
-                using (var responseStreamFromWebSite = responseFromWebSite.GetResponseStream())
-                {
-                    var originalResponse = requestData.context.Response;
+		public static string[] ReadAllLines(string path)
+		{
+			return File.ReadAllText(path, Encoding.UTF8).Split(NewLine);
+		}
+	}
 
-                    if (responseFromWebSite.ContentType.Contains("application/x-debian-package"))
-                    {
-                        int size = responseStreamFromWebSite.Length;
-                        byte[] byteArray = new byte[size];
-                        responseStreamFromWebSite.Read(byteArray, 0, length);
-                        //save to cache
-                        var stream = new MemoryStream(byteArray);
-                        originalResponse.OutputStream.Write(byteArray, 0, length);
-                    }
-                    else
-                    {
-                        responseStreamFromWebSite.CopyTo(originalResponse.OutputStream);
-                    }
-                    originalResponse.OutputStream.Close();
-                }
-            }
-        }
-    }
+	class Cache
+	{
+		static Dictionary<string, string> cachedfiles = new Dictionary<string, string>();
 
-    public static class ConsoleUtilities
-    {
-        public static void WriteRequest(string info)
-        {
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine(info);
-            Console.ResetColor();
-        }
-        public static void WriteResponse(string info)
-        {
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(info);
-            Console.ResetColor();
-        }
-    }
+		public Cache()
+		{
+			if (!Directory.Exists(CACHE_DIR))
+				Directory.CreateDirectory(CACHE_DIR);
+			if (!File.Exists(CACHE_DICT))
+				save();
+			load();
+		}
 
-    public class RequestState
-    {
-        public readonly HttpWebRequest webRequest;
-        public readonly HttpListenerContext context;
+		string NewID => RANDOM.Next().ToString("x");
 
-        public RequestState(HttpWebRequest request, HttpListenerContext context)
-        {
-            webRequest = request;
-            this.context = context;
-        }
-    }
+		void load()
+		{
+			cachedfiles = new Dictionary<string, string>();
+			foreach (string l in ReadAllLines(CACHE_DICT))
+				cachedfiles.Add(l.Substring(l.FirstIndexOf(' ')), l.Substring(0, l.FirstIndexOf(' ')));
+		}
+
+		public void save()
+		{
+			Stream s = File.Open(CACHE_DICT, FileMode.Create, FileAccess.Write);
+			foreach (KeyValuePair<string, string> file in cachedfiles)
+				s.Write(Encoding.UTF8.GetBytes($"{file.Value} {file.Key}" + NewLine));
+			s.Close();
+		}
+
+		public void addfile(string url, byte[] content)
+		{
+			string id = NewID;
+			while (File.Exists(CACHE_DIR + id))
+				id = NewID;
+			File.WriteAllBytes(CACHE_DIR + id, content);
+			cachedfiles.Add(url, id);
+			save();
+		}
+
+		public bool hasfile(string url) => cachedfiles.ContainsKey(url);
+
+		public string getfile(string url) => CACHE_DIR + cachedfiles[url];
+	}
+
+	class Program
+	{
+		public static Cache cache;
+
+		static void Main(string[] args)
+		{
+			var listener = new HttpListener();
+			listener.Prefixes.Add($"http://*:{PORT}/");
+			cache = new Cache();
+			listener.Start();
+			Console.WriteLine("Listening...");
+			while (true)
+				new Thread(new Relay(listener.GetContext()).ProcessRequest).Start();
+		}
+	}
+
+	class Relay
+	{
+		private readonly HttpListenerContext originalContext;
+
+		public Relay(HttpListenerContext originalContext)
+		{
+			this.originalContext = originalContext;
+		}
+
+		public void ProcessRequest() //if in cache read
+		{
+			string rawUrl = originalContext.Request.RawUrl;
+			Console.WriteLine("Received request for: " + rawUrl);
+			if (cache.hasfile(rawUrl))
+			{
+				Task.Run(async () =>
+				{
+					Stream s = File.Open(cache.getfile(rawUrl), FileMode.Open, FileAccess.Read);
+					await s.CopyToAsync(originalContext.Response.OutputStream);
+					s.Close();
+				});
+			}
+			else
+			{
+				var relayRequest = (HttpWebRequest)WebRequest.Create(rawUrl);
+				relayRequest.KeepAlive = false;
+				relayRequest.Proxy.Credentials = CredentialCache.DefaultCredentials;
+				relayRequest.UserAgent = originalContext.Request.UserAgent;
+
+				var requestData = new RequestState(relayRequest, originalContext);
+				relayRequest.BeginGetResponse(ResponseCallBack, requestData);
+			}
+		}
+
+		static void ResponseCallBack(IAsyncResult asynchronousResult)
+		{
+			var requestData = (RequestState) asynchronousResult.AsyncState;
+			Console.WriteLine("Got response from " + requestData.context.Request.RawUrl);
+			
+			using (var responseFromWebSite = (HttpWebResponse) requestData.webRequest.EndGetResponse(asynchronousResult))
+			{
+				using (var responseStreamFromWebSite = responseFromWebSite.GetResponseStream())
+				{
+					var originalResponse = requestData.context.Response;
+
+					if (responseFromWebSite.ContentType.Contains("application/x-debian-package"))
+					{
+						long size = responseStreamFromWebSite.Length;
+						byte[] byteArray = new byte[size];
+						int j;
+						for (long i = 0; (j = responseStreamFromWebSite.ReadByte()) != -1; i++)
+							byteArray[i] = (byte) j;
+						cache.addfile(requestData.context.Request.RawUrl, byteArray);
+						foreach (byte b in byteArray)
+							originalResponse.OutputStream.WriteByte(b);
+					}
+					else
+					{
+						responseStreamFromWebSite.CopyTo(originalResponse.OutputStream);
+					}
+					originalResponse.OutputStream.Close();
+				}
+			}
+		}
+	}
+
+	class RequestState
+	{
+		public readonly HttpWebRequest webRequest;
+		public readonly HttpListenerContext context;
+
+		public RequestState(HttpWebRequest request, HttpListenerContext context)
+		{
+			webRequest = request;
+			this.context = context;
+		}
+	}
 
 }
